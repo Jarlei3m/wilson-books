2ac311622123fc34588df881132de9b64cafa448
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-03-10 18:53:17 -0300

    chore:Added home page books carousell

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,34 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/components/BooksContainer.module.css';
+import data from '../../books.json';
 
 export function Books() {
+  const [books, setBooks] = useState(data);
+  const [position, setPosition] = useState(0);
+
+  useEffect(() => {
+    const lastPosition = books.length;
+    if (position < 0) {
+      setPosition(lastPosition);
+    }
+    if (position === lastPosition) {
+      setPosition(0);
+    }
+  }, [position, books]);
+
+  useEffect(() => {
+    let carousell = setInterval(() => {
+      setPosition(position + 1);
+    }, 6000);
+
+    return () => clearInterval(carousell);
+  }, [position]);
+
   return (
     <section className={styles.container}>
       <div className={styles.booksContainer}>
-        <div className={styles.book}>
-          <img
-            src='https://static.wixstatic.com/media/a5415c_95fc58e060b14f10acd52ca2f370a942~mv2.jpeg/v1/fill/w_395,h_593,al_c,q_80,usm_0.66_1.00_0.01/WhatsApp%20Image%202020-12-07%20at%2010_57_50_jp.webp'
-            alt=''
-          />
-          <article className={styles.descriptionContainer}>
-            <div>
-              <h1>
-                O mais recente lançamento <br />- A Nova Ordem Do Mal
-              </h1>
-              <small>por Wilson Lassarote</small>
+        {books.map((book, bookIndex) => {
+          const { image, title, author, description, link } = book;
+          return (
+            <div
+              key={bookIndex}
+              className={
+                bookIndex === position
+                  ? `${styles.book} ${styles.activeSlide}`
+                  : bookIndex === position - 1 ||
+                    (position === 0 && bookIndex === books.length - 1)
+                  ? `${styles.book} ${styles.prevSlide}`
+                  : `${styles.book} ${styles.nextSlide}`
+              }
+            >
+              <img src={image} alt='' />
+              <article className={styles.descriptionContainer}>
+                <div>
+                  <h1>{title}</h1>
+                  <small>por {author}</small>
+                </div>
+                <div>
+                  <p>{description}</p>
+                  <a target='_blank' href={link}>
+                    <button>Comprar agora</button>
+                  </a>
+                </div>
+              </article>
             </div>
-            <div>
-              <p>Já disponível para venda!</p>
-              <a href=''>
-                <button>Comprar agora</button>
-              </a>
-            </div>
-          </article>
-        </div>
+          );
+        })}
         <nav className={styles.carousellBtns}>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div
+            className={position === 0 ? `${styles.btnActive}` : null}
+            onClick={() => setPosition(0)}
+          ></div>
+          <div
+            className={position === 1 ? `${styles.btnActive}` : null}
+            onClick={() => setPosition(1)}
+          ></div>
+          <div
+            className={position === 2 ? `${styles.btnActive}` : null}
+            onClick={() => setPosition(2)}
+          ></div>
         </nav>
       </div>
     </section>
