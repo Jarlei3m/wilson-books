3279c28480ba901f9a2011b80bb6cf760ecc92a7
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-03-17 11:24:44 -0300

    style: Page author responsive

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import styles from '../styles/components/AuthorContainer.module.css';
-import { AuthorPhoto } from './AuthorPhoto';
-import { AuthorAbout } from './AuthorAbout';
 
 export function Author() {
   return (
     <section id='autor' className={styles.AuthorContainer}>
-      <AuthorPhoto />
-      <AuthorAbout />
+      <div className={styles.photoContainer}>
+        <img src='./wilson.jpg' alt='wilson' />
+      </div>
+      <article className={styles.aboutContainer}>
+        <h1>Sobre mim</h1>
+        <div className={styles.underline}></div>
+        <div>
+          <p>
+            Wilson L.T. Lassarote nascido em 1966 numa pequena cidade do
+            interior do Rio de Janeiro chamada Valença. Sou médico cardiologista
+            e escritor. Atualmente com 3 livros publicados. ​<br />
+            <br />
+            Como autor, acredito que um livro deve deixar marcas e ser agente de
+            transformação e reflexão: "Se você terminou a leitura de um livro da
+            mesma forma que entrou, você perdeu seu tempo ou no máximo teve um
+            passatempo nas mãos".
+          </p>
+        </div>
+      </article>
     </section>
   );
 }
